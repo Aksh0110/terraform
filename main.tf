d999472cdcee72dd7d6a293be8a5873b49e1c1a2
Aksh0110 <140313367+Aksh0110@users.noreply.github.com>
2023-11-03 18:35:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 # Creating VPC
-resource "aws_vpc" "k8s" {
-  cidr_block = "192.168.0.0/16"
+resource "aws_vpc" "terraform" {
+  cidr_block = "192.100.0.0/16"
 
   tags = {
-    Name = "k8s"
+    Name = "terraform"
   }
 }
 
 resource "aws_security_group" "vpctestsg" {
   name        = "vpctestsg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.k8s.id
+  vpc_id      = aws_vpc.terraform.id
 
   ingress {
     description = "TLS from VPC"
@@ -65,10 +65,36 @@ resource "aws_security_group" "vpctestsg" {
 
 # Creating public subnet
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.k8s.id
-  cidr_block = "192.168.1.0/24"
+  vpc_id     = aws_vpc.terraform.id
+  cidr_block = "192.100.1.0/24"
 
   tags = {
     Name = "public"
   }
+}
+# Creating internet gateway
+resource "aws_internet_gateway" "igw-tf" {
+  vpc_id = aws_vpc.terraform.id
+
+  tags = {
+    Name = "igw-tf"
+  }
+}
+
+# Creating route table for public subnet
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.terraform.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-k8s.id
+  }
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
