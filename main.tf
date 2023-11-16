85ecc8e0cbfac080e5ca97381dada369932806e5
Aksh0110 <140313367+Aksh0110@users.noreply.github.com>
2023-11-16 19:54:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ terraform {
     }
   }
 }
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-05c13eab67c5d8861" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "demo-server"
+  }
+}
