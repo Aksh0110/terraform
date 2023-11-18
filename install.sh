82b7bb58677480e44c1df8c0c4e9c35a4cd0f319
Aksh0110 <140313367+Aksh0110@users.noreply.github.com>
2023-11-18 12:43:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install git -y
-sudo yum install -y yum-utils
+sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
-terraform -help
-
